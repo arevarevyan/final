31da0c645b4356dd2f2f3b6166d566eaa809832b
Your Name <you@example.com>
2018-05-27 01:16:12 +0400

    sdvdvdscsdcsdcdsc

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 var express = require("express");
 var app = express();
 
+app.set('port', process.env.PORT || 3000);
+
 app.use(express.static("public"));
 
 app.get("/", function(req, res){
    res.redirect("public");
 });
 
-app.listen(3000, function(){
+app.listen(app.get('port'), function(){
    console.log("Example is running on port 3000");
 });
